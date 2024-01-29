26b482c9679acf6a82615b78af854f3b04ca1935
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-01-29 19:38:26 +0100

    minor fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-};
+import { authOptions } from '@/app/helpers/authOptions';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
