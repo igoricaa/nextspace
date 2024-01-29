39d7ba36696472a6cc8f5fbd0e41518170485517
Igor Stanisavljevic <igorica@Igors-MacBook-Pro.local>
2024-01-29 19:19:00 +0100

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export const dynamic = 'force-static';
+
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'We are a social media company',
+};
+
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>We are a social meda company!</p>
+    </main>
+  );
+}
